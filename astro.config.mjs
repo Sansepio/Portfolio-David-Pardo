9334d1533d1875b67b2714bd881c651adb3110c5
Sansepio <davidtrek800@gmail.com>
2025-01-09 01:33:31 -0300

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sansepio.github.io',
+  base: 'astro_github_pages_ejemplo',
   integrations: [tailwind()]
 });
+
